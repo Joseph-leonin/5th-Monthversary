a386eacbb1d787c2230f2384fdbe41c86ff58d69
Joseph-leonin <jericjosephleonin@gmail.com>
2025-05-11 00:57:56 +0300

    Delete relationship_website_v2 directory

--- relationship_website_v2/client/src/pages/index.js
+++
@@ -1,6 +0,0 @@
-import Home from "./Home";
-import Quiz from "./Quiz";
-import Letter from "./Letter";
-import Drawing from "./Drawing";
-import Closing from "./Closing";
-export { Home, Quiz, Letter, Drawing, Closing };
